2f3baae5fa2d89cd2138347cb7f2b3f1b0c9e13c
Tarun Jadhwani <tarunjadhwani@gmail.com>
2015-03-03 21:51:44 +0530

    with backgrounds

---
+++ app/assets/javascripts/old/colors.js
@@ -0,0 +1,10 @@
+/*
+COLOR PALETTE
+Flexbox Fridays Week 2 brought to you by team Lincoln Loop
+https://lincolnloop.com/
+
+Inspired by this pen:
+http://codepen.io/Nyloxz/pen/Bqpdh
+
+Thanks app.coolors.co for being so awesome!
+*/
